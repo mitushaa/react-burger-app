2b59e542473e70d41b0468519c90d912a523e64a
mitushaa <aryamitusha@gmail.com>
2020-10-03 16:49:27 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { render } from '@testing-library/react';
-import React, { Component} from 'react';
-
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component() {
   render() {
   return (
  
-    <div ></div>
+    <div >
+
+   <Layout>
+     <p>Test</p>
+   </Layout>
+    </div>
  
     
   );
